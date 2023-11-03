bf15d94f8e36b21bde3f9a34ac71c1dda7bb3b85
lsanessl <saness729@gmail.com>
2023-11-03 16:43:26 +0300

    main code

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "functions.h"
+
+int main() //works only with code without errors
+{
+	std::string file;
+	std::cout << "Enter a file/directory: ";
+	std::getline(std::cin, file);
+	format(file);
+
+	std::ifstream in;
+	in.open(file);
+	while (!in.is_open())
+	{
+		std::cout << "File or directory doesn't exist. Please, try again: ";
+		std::cin >> file;
+		in.open(file);
+	}
+
+	find_id(in);
+}
